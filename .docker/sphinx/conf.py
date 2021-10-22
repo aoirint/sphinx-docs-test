6247518074430f6c796a10f1a7ab2d4c8dfe3307
aoirint <aoirint@gmail.com>
2021-10-22 11:52:51 +0900

    edit on gitlab

--- .docker/sphinx/conf.py
+++ .docker/sphinx/conf.py
@@ -51,3 +51,12 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    "display_gitlab": True, # Integrate Gitlab
+    "gitlab_user": "aoirint", # Username
+    "gitlab_repo": "sphinx-docs-test", # Repo name
+    "gitlab_version": "main", # Version
+    "conf_py_path": "/", # Path in the checkout to the docs root
+}
+
